709f555dc5d9597ba77030c74678fecde74a6d4e
FlavianMF <flavianmelquiades@gmail.com>
2020-11-12 11:15:05 -0300

    First Commit

---
+++ include/pins.h
@@ -0,0 +1,11 @@
+#ifndef __PINS_H__
+#define __PINS_H__
+
+#define greenLedPin 9
+#define redLedPin 5
+#define yellowLedPin 10
+
+#define potentiometerPin 18
+#define lm35Pin 19
+
+#endif  // __PINS_H__
